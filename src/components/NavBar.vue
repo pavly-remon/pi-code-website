a488177d21744a60b76fe2f09b08ee7394cd3c71
pavly-remon <pavlyr.2019@gmail.com>
2023-09-16 02:36:30 +0300

    Add NavBar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mt-10" style="width: 100%">
+    <ul class="nav-bar">
+      <li v-for="tab in tabs" :class="'nav-bar-tab text-center h-30 sm:h-20 md:text-sm sm:text-xs' + (tab === currTab ? ' active': '')"
+           @click="changeTab(tab)">
+        {{ tab }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      currTab: "Work Experience",
+      tabs: ["Work Experience", "Projects", "Education & Courses"]
+    }
+  },
+  methods: {
+    changeTab(tabName) {
+      this.currTab = tabName;
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.nav-bar {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.nav-bar-tab {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.nav-bar-tab.active, .nav-bar-tab.active:hover{
+  --tw-text-opacity: 1;
+  color: rgb(254 243 199 / var(--tw-text-opacity));
+  border-bottom: 4px solid rgb(254 243 199 / var(--tw-text-opacity));
+  font-weight: bold;
+  transition: ease 0.5s;
+}
+
+.nav-bar-tab:hover {
+  --tw-text-opacity: 1;
+  color: rgb(147 197 253 / var(--tw-text-opacity));
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home-view">
-    <ContactCard :contact-info="contactInfo"/>
+    <div class="top-section p-5 drop-shadow-md">
+      <ContactCard :contact-info="contactInfo"/>
+      <NavBar/>
+    </div>
   </div>
 </template>
 <script>
 import ContactCard from "../components/ContactCard.vue";
+import NavBar from "../components/NavBar.vue";
 
 export default {
   name: "HomeView",
-  components: {ContactCard},
+  components: {NavBar, ContactCard},
 
   data() {
     return {
@@ -28,8 +32,14 @@ export default {
 
 .home-view {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+.top-section{
+  width: 100%;
+  background-color: rgba(128,128,128,0.1);
+  border-radius: 10px;
 }
 
 
