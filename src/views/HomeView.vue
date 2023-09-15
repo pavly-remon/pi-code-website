c79c701857421a3319c1111bb2f50b3844a32cb4
pavly-remon <pavlyr.2019@gmail.com>
2023-09-15 03:07:08 +0300

    HomeView.vue Responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home-view">
-    <img class="rounded-full xl:w-40 xl:h-40 xs:w-24 xs:h-24 border-4 border-amber-100 mb-5" src="../assets/HW_img.jpg"
+    <img class="rounded-full xl:w-40 xl:h-40 xs:w-16 xs:h-16 border-4 border-amber-100 mb-5" src="../assets/HW_img.jpg"
          alt="Profile Picture">
     <p class="xl:text-4xl sm:text-2xl font-bold mb-2 text-amber-100">{{ title }}</p>
     <p class="xl:text-4xl sm:text-2xl font-bold text-blue-300">{{ Position }}</p>
