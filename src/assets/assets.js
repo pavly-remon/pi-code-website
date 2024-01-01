4790c303d8d1071cebcf9e456b07305b577fcdab
Pavly Remon <pavlyremon@gmail.com>
2024-01-01 16:32:11 +0200

    Fix: Image Paths Issue

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,5 +2,5 @@
   "ProfilePicture": "/src/assets/images/HW_img.jpg",
   "PiCode": "/src/assets/icons/dev_logo.png",
   "GithubLogo": "/src/assets/icons/githubLogo.png",
-  "LinkedInLogo": "/src/assets/icons/LinkedInLogo.png"
+  "LinkedInLogo": "../assets/icons/LinkedInLogo.png"
 }
