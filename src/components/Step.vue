65c3c75ff408045bc6d61da63b6f38128f813bc5
Pavly Remon <pavlyremon@gmail.com>
2024-01-26 17:37:49 +0200

    Change Work Experience View

--- src/components/Step.vue
+++ src/components/Step.vue
@@ -1,45 +1,67 @@
 <template>
-  <div class="step grid grid-cols-[25%_3%_23%_48%] xl:grid-cols-[15%_1%_9%_72%] gap-1%">
-    <p class="text-xs xl:text-sm text-center">{{ stepDate }}</p>
-    <div class="relative flex justify-center text-center h-full">
-      <div class="absolute flex justify-center h-3 w-3 xl:h-5 xl:w-5 text-center">
+  <div class="step flex flex-col flex-grow justify-start items-center">
+    <div class="flex">
+      <div class="relative flex justify-center h-5 w-5 xl:h-8 xl:w-8 text-center mb-2">
         <div class="inline-flex absolute h-full w-full rounded-full bg-blue-300 opacity-100"></div>
         <div class="animate-ping inline-flex h-full w-full rounded-full bg-blue-300 opacity-100"></div>
       </div>
-      <div :class="'border-2 border-blue-300 w-min ' + lineLength"></div>
     </div>
-    <p class="text-xs xl:text-base font-bold text-center">{{ stepTitle }}</p>
-
-    <ul v-if="stepDescription && stepDescription.length !== 0" class="description px-4 pt-2 pb-5 xl:px-10 h-[95%]">
+    <div>
+      <p class="text-xs xl:text-sm text-center">{{ stepDate }}</p>
+      <p class="company-title text-md xl:text-2xl font-bold text-center">
+        {{ stepTitle }}
+      </p>
+    </div>
+    <ul v-show="stepDescription && stepDescription.length !== 0" class="description px-4 pt-2 pb-2 xl:px-10">
       <li class="text-xxs xl:text-sm pr-0 xl:pr-48 mb-2 list-disc" v-for="text in stepDescription">{{ text }}</li>
     </ul>
   </div>
+
 </template>
 <script>
+import $ from "jquery";
+import 'material-icons/iconfont/material-icons.css';
+
 export default {
-  name: 'Step',
   props: {
+    index: {type: Number},
     stepDate: {type: String},
     stepTitle: {type: String},
     stepDescription: {type: Array(String)},
-    /**
-     * TailwindCSS Height Class
-     * @values h-...
-     */
     lineLength: {
       type: String, default: "",
 
     }
+  },
+  mounted() {
+    $(".step").eq(this.index).on({
+      'click': () => {
+        if (this.icon === 'arrow_drop_down') {
+          this.icon = 'arrow_drop_up'
+        } else {
+          this.icon = 'arrow_drop_down'
+        }
+        $(".description").eq(this.index).slideToggle("slow")
+
+      }
+    })
+  },
+  data() {
+    return {
+      icon: 'arrow_drop_up',
+    };
   }
 }
 </script>
 <style scoped>
 .step {
   width: 100%;
+  cursor: pointer;
 }
 
 .description {
   background-color: #242424;
   border-radius: 10px;
 }
+
 </style>
