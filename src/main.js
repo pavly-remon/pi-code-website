2f9e16b537ee39792b8399e41e33ce1493172963
Pavly Remon <pavlyremon@gmail.com>
2024-01-27 14:38:03 +0200

    Build Project with React

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
-import {inject} from '@vercel/analytics';
-
-inject();
-createApp(App).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,41 +2,10 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            fontSize: {
-                'xxs': '8px',
-                'xs': '10px',
-                's': '12px',
-            },
-            screens: {
-                'sm': '300px',
-                'md': '640px',
-                'lg': '1024px',
-                'xl': '1280px',
-                '2xl': '1536px',
-            },
-
-            keyframes: {
-                wiggle: {
-                    '0%, 100%': {transform: 'rotate(-3deg)'},
-                    '50%': {transform: 'rotate(3deg)'},
-                },
-                fadeInFromTop: {
-                    '0%': {
-                        transform: 'translateY(-10%)',
-                        opacity: '0',
-                    },
-                    '100%': {
-                        transform: 'translateY(0)',
-                        opacity: '1',
-                    }
-                }
-            }
-        }
-    }
-    ,
+        extend: {},
+    },
     plugins: [],
 }
